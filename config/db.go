207007b09f4aaebe57f060e0095e3b629f580f03
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-10-06 22:01:40 +0300

    fix(config): fixing database not connected

--- config/db.go
+++ config/db.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/faanrm/CRUD-api/config/utils"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	utils.setEnvVar()
+	utils.SetEnvVar()
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
+	db, err := gorm.Open(mysql.Open(uri), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get database connection: %v", err)
+	}
+	err = sqlDB.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	fmt.Println("connected to database")
+	return db, nil
 }
