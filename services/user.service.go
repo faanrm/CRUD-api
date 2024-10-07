cef5d2f99fdac6881e0889b605150ffe86236c98
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-10-07 19:02:07 +0300

    crud operation ok

--- services/user.service.go
+++ services/user.service.go
@@ -5,16 +5,33 @@ import "github.com/faanrm/CRUD-api/models"
 type UserServices struct {
 }
 
-func (us *UserServices) CreateUser(user *models.Users) (*models.Users, error) {
-	createdUser, err := user.Create()
-	if err != nil {
-		return nil, err
-	}
-	return createdUser, nil
-}
-func (us *UserServices) GetUser(userId string) ([]*models.Users, error) {
+func (us *UserServices) CreateUser(user *models.Users, userId string) (*models.Users, error) {
 	if userId == "" {
-		users, err := models.ReadAll()
+		createdUser, err := user.Create()
+		if err != nil {
+			return nil, err
+		}
+		return createdUser, nil
+	} else {
+		existingUser, err := user.Read()
+		if err != nil {
+			return nil, err
+		}
+		existingUser.Name = user.Name
+		existingUser.Username = user.Username
+		existingUser.Email = user.Email
+		existingUser.Phone = user.Phone
+		existingUser.Address = user.Address
+		updatedUser, err := existingUser.Update()
+		if err != nil {
+			return nil, err
+		}
+		return updatedUser, nil
+	}
+}
+func (us *UserServices) GetUser(user *models.Users, userId string) ([]*models.Users, error) {
+	if userId == "" {
+		users, err := user.ReadAll()
 		if err != nil {
 			return nil, err
 		}
@@ -27,4 +44,12 @@ func (us *UserServices) GetUser(userId string) ([]*models.Users, error) {
 		}
 		return []*models.Users{foundUser}, nil
 	}
+}
+func (us *UserServices) DeleteUser(userId string) error {
+	user := &models.Users{UserId: userId}
+	err := user.Delete()
+	if err != nil {
+		return err
+	}
+	return nil
 }
