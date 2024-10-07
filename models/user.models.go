d510a2346bebd6ab65ad15350ed6b9ee77e91aa3
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-10-07 18:52:07 +0300

    feat : creat models

--- models/user.models.go
+++ models/user.models.go
@@ -1,1 +1,78 @@
 package models
+
+import (
+	"time"
+
+	"github.com/faanrm/CRUD-api/config"
+)
+
+type Users struct {
+	UserId    string `gorm:"primary_key" json:"userId"`
+	Name      string `gorm:"type:varchar(255);NOT NULL" json:"name" binding:"required"`
+	Username  string `gorm:"type:varchar(255);NOT NULL" json:"username"`
+	Email     string `gorm:"type:varchar(255);UNIQUE_INDEX" json:"email" binding:"required"`
+	Phone     string `gorm:"type:varchar(100);NOT NULL;UNIQUE;UNIQUE_INDEX" json:"phone" binding:"required"`
+	Address   string `gorm:"type:text" json:"address"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (u *Users) Create() (*Users, error) {
+	db, err := config.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	err = db.Create(&u).Error
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+func (u *Users) ReadGeAll() ([]*Users, error) {
+	db, err := config.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	var users []*Users
+	err = db.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (u *Users) Read() (*Users, error) {
+	db, err := config.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	err = db.Where("user_id = ?", u.UserId).First(&u).Error
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+func (u *Users) Update() (*Users, error) {
+	db, err := config.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	err = db.Save(&u).Error
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+func (u *Users) Delete() error {
+	db, err := config.ConnectDB()
+	if err != nil {
+		return err
+	}
+	err = db.Where("user_id = ?", u.UserId).Delete(&u).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- services/user.service.go
+++ services/user.service.go
@@ -1,1 +1,32 @@
 package services
+
+import "github.com/faanrm/CRUD-api/models"
+
+type UserServices struct {
+}
+
+func (us *UserServices) CreateUser(user *models.Users) (*models.Users, error) {
+	createdUser, err := user.Create()
+	if err != nil {
+		return nil, err
+	}
+	return createdUser, nil
+}
+func (us *UserServices) GetUser(userId string) ([]*models.Users, error) {
+	if userId == "" {
+		// If no ID is specified, retrieve all users
+		users, err := models.ReadAll()
+		if err != nil {
+			return nil, err
+		}
+		return users, nil
+	} else {
+		// If an ID is specified, retrieve the user with that ID
+		user := &models.Users{UserId: userId}
+		foundUser, err := user.Read()
+		if err != nil {
+			return nil, err
+		}
+		return []*models.Users{foundUser}, nil
+	}
+}
